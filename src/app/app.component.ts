37c9ab16cc70cb901a015eab5932ab54f5b74152
Suman Sharma <suman.sharma0331@gmail.com>
2022-04-15 13:28:47 +0530

    1. creating an observable, observer and subscribing to it

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { Observable } from 'rxjs';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'practise-app';
+export class AppComponent implements OnInit{
+
+
+ ngOnInit(): void {
+   this.observable$.subscribe(this.observer);
+ }
+
+ observable$=new Observable((subscriber)=>{
+   console.log("observable executed")
+   subscriber.next("Suman");
+   subscriber.next("pujan");
+ })
+ observer={
+   next:(value:any)=>{console.log(value);}
+ }
 }
